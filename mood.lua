7bed5979722a996a61c8b045de788798c8e78f5e
Jankly <iit2007@hotmail.com>
2010-10-26 15:37:32 -0400

    Simple addon working.

--- mood.lua
+++ mood.lua
@@ -1,13 +1,25 @@
+-- Setup mood as a global and grab a copy of the version # set in the .toc
 local parent, ns = ...
-local version = GetAddOnMetadata(parent, 'version')
+local _VERSION = GetAddOnMetadata(parent, 'version')
+local global = GetAddonMetadata(parent, 'X=mood')
 
+local mood = {}
+
+-- Local variables
 local dummy = function() end
-
 local playerClass = select(2, UnitClass("player"))
 local playerName = UnitName("Player")
 local playerRealm = GetRealmName()
+local newCharacter = true
 
-local output = {
+-- 
+mood.frames = {}
+mood.strings = {}
+mood.outputMethods = {}
+
+
+
+mood.strings = {
 	['guild'] = playerName .. "'s mood is currently set to %s.",
 	['whisper'] = "Is currently in a %s mood.",
 	['party'] = "I'm currently in a %s mood.",
@@ -18,24 +30,45 @@ local whisper = {
 	"Keltric", "Belliofria", "Jankly", -- addon author (Bell) used for testing
 }
 
+---------------------------------------
+-- Stuff to hanlde loading the addon --
+---------------------------------------
 
-local mood = CreateFrame"Frame"
+local mood.loader = CreateFrame"Frame"
 
-local OnEvent = function(self, event, arg1, ...)
-	if (event == "ADDON_LOADED" and arg1 == "currentMood") then
-		if currentMood == nil then
-			currentMood = "Happy"	
-		else
-			for player in ipairs(whisper) do
-				if UnitIsConnected(player) then
-					SendChatMessage(format(output['whisper'], currentMood), "WHISPER", nil, player)
-				end
+local OnEvent = function(self, event, ...)
+	if event == "ADDON_LOADED" then
+		local arg1 = ...
+		if arg1 == "currentMood" and currentMood == nil then
+			currentMood = "Happy"				
+			-- Fire mood frame to set new mood.
+		end
+	elseif event == "PLAYER_ENTERING_WORLD" then
+		for _, player in ipairs(whisper) do
+			print(player)
+			if UnitIsConnected(player) then
+				SendChatMessage(format(output['whisper'], currentMood), "WHISPER", nil, player)
 			end
 		end
 	end
 end
 
-mood:RegisterEvent("ADDON_LOADED") -- Used to pull back previous mood.
-mood:RegisterEvent("PLAYER_ENTERING_WORLD") -- Used to set the new mood for first loads into the world.
+mood.loader:RegisterEvent("ADDON_LOADED") -- Used to pull back previous mood.
+mood.loader:RegisterEvent("PLAYER_ENTERING_WORLD") -- Used to set the new mood for first loads into the world.
 
-mood:SetScript("OnEvent", OnEvent)
+mood.loader:SetScript("OnEvent", OnEvent)
+
+--------------------
+-- Globalize mood --
+--------------------
+-- Jacked from haste/oUF
+mood.version = _VERSION
+
+if (global) then
+	if (parent ~= "mood" and global == "mood") then
+		error("%s is doing it wrong and setting its global to mood.", parent)
+	else
+		_G[global] = oUF
+	end
+end
+ns.mood = mood
